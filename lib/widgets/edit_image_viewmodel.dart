0e855dfa2669f863e5f21ccec071cd325d4934a1
Soumik Roy Chowdhury <eventssoumik01@gmail.com>
2024-06-05 00:03:34 +0530

    app bar with many features

--- lib/widgets/edit_image_viewmodel.dart
+++ lib/widgets/edit_image_viewmodel.dart
@@ -5,7 +5,82 @@ import 'package:image_editor/widgets/default_button.dart';
 
 abstract class EditImageViewModel extends State<EditImageScreen> {
   TextEditingController textEditingController = TextEditingController();
+  TextEditingController creatorText = TextEditingController();
   List<TextInfo> texts=[];
+  int currentIndex=0;
+  setCurrentIndex(BuildContext context,index){
+    setState((){
+      currentIndex=index;
+    });
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content:Text('Selected For Styling',style:TextStyle(fontSize:16))));
+  }
+  changeTextColor(Color color){
+    setState((){
+      texts[currentIndex].color=color;
+    });
+  }
+  increaseFontSize() {
+    setState(() {
+      texts[currentIndex].fontSize++;
+    });
+  }
+
+  decreaseFontSize() {
+    setState(() {
+      texts[currentIndex].fontSize--;
+    });
+  }
+
+  alignLeft() {
+    setState(() {
+      texts[currentIndex].textAlign = TextAlign.left;
+    });
+  }
+
+  alignCenter() {
+    setState(() {
+      texts[currentIndex].textAlign = TextAlign.center;
+    });
+  }
+
+  alignRight() {
+    setState(() {
+      texts[currentIndex].textAlign = TextAlign.right;
+    });
+  }
+
+  boldText() {
+    setState(() {
+      if (texts[currentIndex].fontWeight == FontWeight.bold) {
+        texts[currentIndex].fontWeight = FontWeight.normal;
+      } else {
+        texts[currentIndex].fontWeight = FontWeight.bold;
+      }
+    });
+  }
+
+  italicText() {
+    setState(() {
+      if (texts[currentIndex].fontStyle == FontStyle.italic) {
+        texts[currentIndex].fontStyle = FontStyle.normal;
+      } else {
+        texts[currentIndex].fontStyle = FontStyle.italic;
+      }
+    });
+  }
+
+  addLinesToText() {
+    setState(() {
+      if (texts[currentIndex].text.contains('\n')) {
+        texts[currentIndex].text =
+            texts[currentIndex].text.replaceAll('\n', ' ');
+      } else {
+        texts[currentIndex].text =
+            texts[currentIndex].text.replaceAll(' ', '\n');
+      }
+    });
+  }
   addNewText(BuildContext context){
     setState((){
       texts.add(TextInfo(text: textEditingController.text, left: 0, top: 0, color: Colors.black, fontWeight: FontWeight.normal, fontStyle: FontStyle.normal, fontSize: 20, textAlign: TextAlign.left));
